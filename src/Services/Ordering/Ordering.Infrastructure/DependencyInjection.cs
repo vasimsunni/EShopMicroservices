f8a77ffb6f4bda2ed5c6c5236746933a7b9abf3a
unknown <sunni.vasim@gmail.com>
2024-06-30 19:25:13 +0400

    Ordering.Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddScoped<ISaveChangesInterceptor,AuditableEntityInterceptor>();   
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetService<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString);
+            });
+
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Ordering.Domain.Models;
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Infrastructure.Data;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Domain.Abstraction;
+global using Ordering.Infrastructure.Data.Interceptors;
